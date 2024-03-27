c4dd808ab1bced7246a38adfefe75e57322c4911
AltafAlam Shaikh <gitforaltaf@gmail.com>
2024-03-27 22:47:00 +0530

    api integration trending and coin page created with dummy data

---
+++ src/components/CoinChart.jsx
@@ -0,0 +1,89 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import useThemeContext from "../context/theme";
+import { Box, Button, ButtonGroup, Stack } from "@mui/material";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const CoinChart = () => {
+  const { mode } = useThemeContext();
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Historical Chart",
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          color: mode === "dark" ? "#222" : "#ccc",
+        },
+      },
+      y: {
+        grid: {
+          color: mode === "dark" ? "#222" : "#ccc",
+        },
+      },
+    },
+  };
+
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: labels.map(() => Math.floor(Math.random() * 1000 + 1)),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Dataset 2",
+        data: labels.map(() => Math.floor(Math.random() * 1000 + 1)),
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+
+  return (
+    <>
+      <Line options={options} data={data} />
+    </>
+  );
+};
+
+export default CoinChart;
--- src/pages/Coin.jsx
+++ src/pages/Coin.jsx
@@ -1,7 +1,73 @@
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Container,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import { StyledTypography } from "../components/styledComponents/typography";
+import CoinChart from "../components/CoinChart";
 
 const Coin = () => {
-  return <div>Coin</div>;
+  return (
+    <section className="coin-page" style={{ padding: "4rem 0" }}>
+      <Container sx={{ textAlign: { xs: "center", sm: "left" } }}>
+        <Box>
+          <Grid container>
+            <Grid item xs={12} sm={3}>
+              <Box sx={{ height: { xs: "150px", sm: "auto" } }}>
+                <img
+                  src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400"
+                  alt="coin"
+                  style={{ maxHeight: "100%" }}
+                />
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={9}>
+              <Stack spacing={2}>
+                <StyledTypography variant="h2">Bitcoin</StyledTypography>
+                <Typography variant="body1">
+                  Bitcoin is the first successful internet money based on
+                  peer-to-peer technology; whereby no central bank or authority
+                  is involved in the transaction and production of the Bitcoin
+                  currency.
+                </Typography>
+                <Stack>
+                  <Typography>
+                    <strong>Rank: </strong>1
+                  </Typography>
+                  <Typography>
+                    <strong>Current Price: </strong>₹ 5,744,807
+                  </Typography>
+                  <Typography>
+                    <strong>Market Cap: </strong>₹ 112,789,080M
+                  </Typography>
+                </Stack>
+              </Stack>
+            </Grid>
+          </Grid>
+        </Box>
+        {/* chart */}
+        <Box mt={8} pb={4} height="550px">
+          <CoinChart />
+          <Stack mt={5} alignItems="center">
+            <ButtonGroup
+              variant="contained"
+              color="secondary"
+              aria-label="Day wise chart"
+            >
+              <Button>One</Button>
+              <Button>Two</Button>
+              <Button>Three</Button>
+            </ButtonGroup>
+          </Stack>
+        </Box>
+      </Container>
+    </section>
+  );
 };
 
 export default Coin;
